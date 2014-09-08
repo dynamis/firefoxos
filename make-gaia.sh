c48f333251377e7dc37996277a4878f57d675fc5
dynamis <dynamis@firehacks.org>
2014-09-08 23:30:34 +0900

    support command like 'make-gaia.sh reset-gaia' (without branch name)

--- make-gaia.sh
+++ make-gaia.sh
@@ -8,26 +8,38 @@ case "$1" in
   export LOCALE_BASEDIR="$userconfig_dir/$GAIALOCALEBASEDIR"
   export L10NBASEDIR="$userconfig_dir/$GECKOL10NBASEDIR"
   cd ./$GAIADIR
+  shift
+  make $*
   ;;
 
 "v2.0"|"2.0")
   export LOCALE_BASEDIR="$userconfig_dir/$GAIA20LOCALEBASEDIR"
   export L10NBASEDIR="$userconfig_dir/$GECKO32L10NBASEDIR"
   cd ./$GAIA20DIR
+  shift
+  make $*
   ;;
 
 "v1.4"|"1.4")
   export LOCALE_BASEDIR="$userconfig_dir/$GAIA14LOCALEBASEDIR"
   export L10NBASEDIR="$userconfig_dir/$GECKO30L10NBASEDIR"
   cd ./$GAIA14DIR
+  shift
+  make $*
   ;;
 
 "v1.3"|"1.3")
   export LOCALE_BASEDIR="$userconfig_dir/$GAIA13LOCALEBASEDIR"
   export L10NBASEDIR="$userconfig_dir/$GECKO28L10NBASEDIR"
   cd ./$GAIA13DIR
+  shift
+  make $*
+  ;;
+
+*)
+  export LOCALE_BASEDIR="$userconfig_dir/$GAIALOCALEBASEDIR"
+  export L10NBASEDIR="$userconfig_dir/$GECKOL10NBASEDIR"
+  cd ./$GAIADIR
+  make $*
   ;;
 esac
-
-shift
-make $*
